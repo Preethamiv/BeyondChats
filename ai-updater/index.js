3808bb8218711915851e26578d112616222f0e6c
preethamiv <immadi.pree1204@gmail.com>
2025-12-29 23:27:32 +0530

    gemini working finally!

--- ai-updater/index.js
+++ ai-updater/index.js
@@ -10,7 +10,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Gemini setup 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
